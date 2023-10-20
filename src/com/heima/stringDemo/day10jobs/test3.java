31c7423ba675242726be89d1037959b553a0aec6
harvey1003 <1428009419@qq.com>
2023-10-20 16:05:12 +0800

    md

--- src/com/heima/stringDemo/day10jobs/test3.java
+++ src/com/heima/stringDemo/day10jobs/test3.java
@@ -1,4 +1,18 @@
 package com.heima.stringDemo.day10jobs;
 
+import java.util.Random;
+
 public class test3 {
+    public static void main(String[] args) {
+        //tochararr ´òÂÒ×Ö·û´®
+        String str="abcdefg";
+        Random random = new Random();
+        char[] chars = str.toCharArray();
+        System.out.println(chars.length);
+        for (int i = 0; i < chars.length; i++) {
+            chars[i]=chars[random.nextInt(chars.length)];
+        }
+        String s = new String(chars);
+        System.out.println(s);
+    }
 }
---
+++ src/com/heima/stringDemo/day10jobs/test4.java
@@ -0,0 +1,11 @@
+package com.heima.stringDemo.day10jobs;
+
+
+public class test4 {
+    public static void main(String[] args) {
+        //长度为5，大小写字母数字。四个字母一个数字。数字可以出现在任意位置
+        //char[] daxie={qwertyuiopasdfghjklzxcvbnm};
+        char[] zimu ={'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'};
+        System.out.println(zimu[0]);
+    }
+}
